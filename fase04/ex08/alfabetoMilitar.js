6eff62480e6e1fecf76ec377ca1596103ff76f9a
julialga <52722906+julialga@users.noreply.github.com>
2019-07-17 20:10:43 -0300

    Fase04/Ex08

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,20 @@
+function alfabetoMilitar(v) {
+  var letra = v.toUpperCase()
+   if (letra == "A") {
+  return "alpha";
+   }
+  if (letra =="E") {
+  return "echo";
+  }
+  if (letra =="I") {
+  return "india";
+   } 
+    if (letra == "O") {
+       return "oscar";
+   }
+ if (letra == "U") {
+       return "uniform";  
+ }else{
+    return false
+ }
+}
