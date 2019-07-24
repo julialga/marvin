c982c1a3070eb2c9f7389b6cc0f63ca821d403a9
julialga <52722906+julialga@users.noreply.github.com>
2019-07-24 20:14:09 -0300

    Fase05/Ex07

--- fase05/ex07/quantasLetras.js
+++ fase05/ex07/quantasLetras.js
@@ -1,5 +1,5 @@
 function quantasLetras(array) {
    var quantidade = array.length - 1
    var string = array.join([separador = ',']);
-    return string.length-quantidade;
+    return string.length - quantidade;
 }
