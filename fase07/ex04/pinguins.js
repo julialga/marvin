6e57e2774334faeeab96b174b5f346f7d22f4153
julialga <52722906+julialga@users.noreply.github.com>
2019-08-13 21:22:59 -0300

    pinguin adiciona propriedade de objetos

---
+++ fase07/ex04/pinguins.js
@@ -0,0 +1,9 @@
+function pinguins(array) {
+   var p = 0    
+    while (p <array.length) {
+       t = {cabeca: "oculos de sol", camisa: "jaqueta polar", pes: "tenis"};
+      array[p].roupa=t
+       p++
+   }
+      return array;     
+}
